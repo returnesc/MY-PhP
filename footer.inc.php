613a92887c9cf53059a9040398443a987dbd9fbc
returnesc <82842953+returnesc@users.noreply.github.com>
2021-04-19 23:27:41 +0300

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,4 @@
+<div class="footer">
+            <h2>Мы ОЩУПАЛИ основы PHP!</h2>
+            <img src="img/tuch.gif" width="300" height="200" align="right" vspace="5" hspace="5">
+        </div>
---
+++ logo.inc.php
@@ -0,0 +1,3 @@
+<div class="logo"> 
+                <img src="img/images.png" alt="php">
+                </div>    
